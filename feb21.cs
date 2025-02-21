3d69cfbdf9b28c0af643850d01975c7435e828d9
jordan1013 <jordant917@stemcollegiate.ca>
2025-02-21 08:55:00 -0700

    Create feb21.cs

---
+++ feb21.cs
@@ -0,0 +1,4 @@
+string firstName = "Ahmad";
+int number = 85;
+double temp = 109.9;
+Console.WriteLine($"Hello, {firstName}! You have {number} messages in your inbox. The temperature is {temp} celsius.");
