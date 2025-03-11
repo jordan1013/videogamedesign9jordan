759f8d54c91c3bc6d1a5346b5f0013a471f8c6b0
jordan1013 <jordant917@stemcollegiate.ca>
2025-03-11 12:46:43 -0600

    Create mar11.cs

---
+++ mar11.cs
@@ -0,0 +1,15 @@
+Today I did module 4 lesson 1 in Microsoft Learn. This is what I learned about. 
+
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.WriteLine(@"    c:\source\repos    
+        (this is where your code goes)");
+        Console.Write(@"c:\invoices");
+        
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// User command to run an application
+Console.WriteLine(@"c:\invoices\app.exe -j");
