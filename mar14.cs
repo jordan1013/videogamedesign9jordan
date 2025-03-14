54e5ce6ce21c97757a8a092866c141dd83a0936f
jordan1013 <jordant917@stemcollegiate.ca>
2025-03-14 09:39:47 -0600

    Create mar14.cs

---
+++ mar14.cs
@@ -0,0 +1,7 @@
+//Today I did Unit 3 and 4 in Module 4.
+string firstName = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
+//The code above was from Unit 3 and the code below is from Unit 4. 
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
