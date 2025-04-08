1ef8e8141f794c45fcadf9c71a80b2ca928112cb
jordan1013 <jordant917@stemcollegiate.ca>
2025-04-08 12:24:27 -0600

    Create apr8.cs

---
+++ apr8.cs
@@ -0,0 +1,12 @@
+//Today, I did units 5-8 in Module 4 in Microsoft Learn. 
+
+//Unit 5:
+string projectName = "ACME";
+string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+
+//The above code was from units 5 and 6, and unit 7 was an assessment, which I got 100% on. Unit 8 was a summary of the module. 
