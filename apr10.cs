ea2569fe641e605c8af6261e931c87102c3406bf
jordan1013 <jordant917@stemcollegiate.ca>
2025-04-10 10:09:58 -0600

    Create apr10.cs

---
+++ apr10.cs
@@ -0,0 +1,5 @@
+//I did unit 2 in module 5 in Microsoft Learn and I learned how to perform addition with implicit data conversion. 
+
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
